866eaa549e26fbd326a3cbb2e971ff9ecef546f7
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-03-26 00:09:54 +0100

    Ajout du menu burger fonctionnel + Responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,22 @@
-$( document ).ready(function() {
-  $(".burg").click(function(){
-    $(".menu-mobile").css("transform","translateY(0)");
+$(document).ready(function () {
+  // Ouvrir le menu
+
+  $(".burg").click(function () {
+    $(".menu-mobile").css("transform", "translateY(0)");
     $(".burg").hide();
+    $(".cross").show();
   });
-  $(".all").click(function(){
-$(".menu-mobile").css("transform","translateY(-100%)")
+
+  // Fermer le menu
+  $(".cross").click(function () {
+    $(".menu-mobile").css("transform", "translateY(-1000px)");
+    $(".cross").hide();
+    $(".burg").show();
+  });
+
+  $(".all").click(function () {
+    $(".menu-mobile").css("transform", "translateY(-100%)");
+    $(".cross").hide();
+    $(".burg").show();
   });
 });
